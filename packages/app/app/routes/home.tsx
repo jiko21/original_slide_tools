8a7cbfee6c103efa6be983e9246b4aaa3b04b707
jiko21 <sos.daiki.keion@gmail.com>
2025-03-22 23:53:37 +0900

    add: markdown preview

--- packages/app/app/routes/home.tsx
+++ packages/app/app/routes/home.tsx
@@ -1,3 +1,9 @@
+import rehypeStringify from 'rehype-stringify';
+import remarkFrontmatter from 'remark-frontmatter';
+import remarkGfm from 'remark-gfm';
+import remarkParse from 'remark-parse';
+import remarkRehype from 'remark-rehype';
+import { unified } from 'unified';
 import type { Route } from "./+types/home";
 
 export function meta({ }: Route.MetaArgs) {
@@ -7,6 +13,28 @@ export function meta({ }: Route.MetaArgs) {
   ];
 }
 
-export default function Home() {
-  return <div className="text-amber-300">test</div>;
+export async function loader() {
+  const samples = `# sample
+this is sample
+
+---
+# test
+aaaa`;
+  const file = await unified()
+    .use(remarkParse)
+    .use(remarkFrontmatter)
+    .use(remarkGfm)
+    .use(remarkRehype)
+    .use(rehypeStringify)
+    .process(samples)
+  const splitFiles = String(file).split('<hr>');
+  return {
+    splitFiles,
+  };
+}
+
+export default function Home({
+  loaderData
+}: Route.ComponentProps) {
+  return <div className="text-amber-300" dangerouslySetInnerHTML={{ __html: loaderData.splitFiles }} />;
 }
