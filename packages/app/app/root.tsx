4765414d51ef42816228947b701bf49771c37d8f
jiko21 <sos.daiki.keion@gmail.com>
2025-03-23 22:50:35 +0900

    update: add style for printing

--- packages/app/app/root.tsx
+++ packages/app/app/root.tsx
@@ -32,7 +32,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Meta />
         <Links />
       </head>
-      <body>
+      <body className="m-0 w-screen h-screen bg-black relative">
         {children}
         <ScrollRestoration />
         <Scripts />
