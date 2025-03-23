cec6508900dc32f2e06a52b4f2aa4596e9006261
jiko21 <sos.daiki.keion@gmail.com>
2025-03-23 22:12:21 +0900

    update: use slide component

--- packages/app/app/routes/home.tsx
+++ packages/app/app/routes/home.tsx
@@ -1,10 +1,13 @@
 import { readFile } from 'fs/promises';
+import { useCallback, useEffect } from 'react';
+import { useSearchParams } from 'react-router';
 import rehypeStringify from 'rehype-stringify';
 import remarkFrontmatter from 'remark-frontmatter';
 import remarkGfm from 'remark-gfm';
 import remarkParse from 'remark-parse';
 import remarkRehype from 'remark-rehype';
 import { unified } from 'unified';
+import { Slide } from '~/components/Slide';
 import type { Route } from "./+types/home";
 
 export function meta({ }: Route.MetaArgs) {
@@ -32,5 +35,46 @@ export async function loader() {
 export default function Home({
   loaderData
 }: Route.ComponentProps) {
-  return <div className="text-amber-300" dangerouslySetInnerHTML={{ __html: loaderData.splitFiles }} />;
+  const [searchParams, setSearchParams] = useSearchParams();
+  const activeIndex = Number(searchParams.get('p') ?? '1') - 1;
+  const onKeyDown = useCallback((e: KeyboardEvent) => {
+    const pages = Number(searchParams.get('p') ?? '1');
+    switch (e.key) {
+      case 'ArrowLeft':
+      case 'ArrowUp':
+        if (pages === 1) {
+          break;
+        }
+
+        setSearchParams((prev) => {
+          prev.set('p', String(pages - 1));
+          return prev;
+        });
+        break;
+      case 'ArrowRight':
+      case 'ArrowDown':
+        if (pages === loaderData.splitFiles.length) {
+          break;
+        }
+        setSearchParams((prev) => {
+          prev.set('p', String(pages + 1));
+          return prev;
+        });
+        break;
+      default:
+        break;
+    }
+  }, [searchParams, setSearchParams, loaderData.splitFiles]);
+
+  useEffect(() => {
+    document.addEventListener('keydown', onKeyDown);
+    return () => {
+      document.removeEventListener('keydown', onKeyDown);
+    };
+  }, [onKeyDown]);
+  return <div className="relative">
+    {loaderData.splitFiles.map((item, index) => (
+      <Slide isActive={index === activeIndex} item={item} key={`${item}-${index}`} />
+    ))}
+  </div>;
 }
