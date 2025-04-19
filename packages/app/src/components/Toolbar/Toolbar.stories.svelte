720d400ed695cfda0fb8ef95ab1fa3faba2dace5
jiko21 <sos.daiki.keion@gmail.com>
2025-04-19 22:48:50 +0900

    add: toolbar component

---
+++ packages/app/src/components/Toolbar/Toolbar.stories.svelte
@@ -0,0 +1,25 @@
+<script module>
+	import { defineMeta } from '@storybook/addon-svelte-csf';
+	import Toolbar from './Toolbar.svelte';
+
+	// More on how to set up stories at: https://storybook.js.org/docs/writing-stories
+	const { Story } = defineMeta({
+		title: 'Components/Toolbar',
+		component: Toolbar,
+		// This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
+		tags: ['autodocs'],
+		parameters: {
+			// More on how to position stories at: https://storybook.js.org/docs/configure/story-layout
+			layout: 'fullscreen'
+		},
+		args: {
+			// onLogin: fn(),
+			// onLogout: fn(),
+			// onCreateAccount: fn()
+		}
+	});
+</script>
+
+<Story name="Logged In" />
+
+<Story name="Logged Out" />
---
+++ packages/app/src/components/Toolbar/Toolbar.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import ArrowBack from '../icons/ArrowBack.svelte';
+	import ArrowForward from '../icons/ArrowRight.svelte';
+	import FullScreen from '../icons/FullScreen.svelte';
+</script>
+
+<div class="w-fit rounded-full bg-black p-2 opacity-50">
+	<button class="p-1 hover:opacity-20">
+		<ArrowBack />
+	</button>
+	<button class="p-1 hover:opacity-20">
+		<FullScreen />
+	</button>
+	<button class="p-1 hover:opacity-20">
+		<ArrowForward />
+	</button>
+</div>
